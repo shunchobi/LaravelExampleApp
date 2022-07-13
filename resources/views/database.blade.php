90d1ede076470dfc957d0876b60204cd89a66903
shunchobi <shunbeat1008@gmail.com>
2022-07-13 17:36:05 +0900

    first

---
+++ resources/views/database.blade.php
@@ -0,0 +1,11 @@
+<strong>Database Connected: </strong>
+    <?php
+        try {
+            \DB::connection()->getPDO();
+            echo \DB::connection()->getDatabaseName();
+            } catch (\Exception $e) {
+            echo 'None';
+        }
+    ?>
+
+
